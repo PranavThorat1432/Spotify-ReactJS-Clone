42d9ca322cf343a23244bcb423239b6b0ae2f76d
PranavThorat1432 <pranavthorat95@gmail.com>
2025-07-23 11:16:52 +0530

    Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import PlayerContextProvider from './Context/PlayerContext.jsx'
+import ToastProvider from './Context/ToastContext.jsx'
+import ThemeProvider from './Context/ThemeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <PlayerContextProvider>
+          <ToastProvider>
+            <App />
+          </ToastProvider>
+        </PlayerContextProvider>
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
