c1a52ed2d0e14c903129207b50e269d7a492df9d
PranavThorat1432 <pranavthorat95@gmail.com>
2025-07-23 11:16:28 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useContext } from 'react'
+import Sidebar from './MyComponents/Sidebar'
+import Player from './MyComponents/Player'
+import Display from './MyComponents/Display'
+import { PlayerContext } from './Context/PlayerContext'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
+  const {audioRef, track} = useContext(PlayerContext);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='h-screen bg-white dark:bg-black transition-colors duration-300'>
+      <div className='h-[90%] flex'>
+        <Sidebar/>
+        <Display/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Player/>
+      <audio ref={audioRef} src={track.file} preload='auto'></audio>
+    </div>
   )
 }
 
